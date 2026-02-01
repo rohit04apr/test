d05aa6de541a571e119cd84dea10f7c9864e8af5
Rohit Tiwari <rohit.tiwari@ukg.com>
2026-02-01 12:38:09 +0530

    test

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Use the route() decorator to tell Flask what URL should trigger the function
+@app.route("/")
+def hello_world():
+    return "Facebook by Shruti!!!!!!"
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
+
